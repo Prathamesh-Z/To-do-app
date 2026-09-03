85e2209e35cf41fe60f8b39f0cd51d86b0ea8044
Prathamesh-Z <194089299+Prathamesh-Z@users.noreply.github.com>
2026-09-03 13:51:13 +0530

    add feature (complete & delete task)

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ btn.addEventListener("click", ()=> {
     uList.appendChild(li);
     inp.value = "";
 
+    let editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    editBtn.classList.add("edit");
+    li.appendChild(editBtn);
+
     let delBtn = document.createElement("button");
     delBtn.innerText = "Delete";
     delBtn.classList.add("delete");
@@ -19,8 +24,46 @@ btn.addEventListener("click", ()=> {
 let ul = document.querySelector("ul");
 
 ul.addEventListener("click", function(event){
-    if(event.target.nodeName == "BUTTON"){
+    if(event.target.classList == "delete"){
         let par = event.target.parentElement;
         par.remove();
+        // console.log(event.target); it returns the button element
+    }
+
+    if(event.target.classList == "edit"){
+        let par = event.target.parentElement;
+
+        // let inp = document.querySelector("input");
+        // let uList = document.querySelector("ul");
+
+        let editInput = document.createElement("input");
+        editInput.placeholder = "edit task";
+        let changeBtn = document.createElement("button");
+        changeBtn.innerText = "edit";
+        par.appendChild(editInput);
+        par.appendChild(changeBtn);
+
+        changeBtn.addEventListener("click", ()=> {
+            par.innerText = editInput.value;
+
+            let editBtn = document.createElement("button");
+            editBtn.innerText = "Edit";
+            editBtn.classList.add("edit");
+            par.appendChild(editBtn);
+
+            let delBtn = document.createElement("button");
+            delBtn.innerText = "Delete";
+            delBtn.classList.add("delete");
+            par.appendChild(delBtn);
+        })
+
+        // console.log(par); returns current li
+    }
+    
+});
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName == "LI"){
+    event.target.classList.toggle('completed');
     }
 });
